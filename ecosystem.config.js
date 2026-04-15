45fab4d1ca62c1c0810e0e08c0b2742bf48aee0d
BabayomiDev <mac@Oluwasuyis-Mac-mini.local>
2026-04-15 16:21:54 +0100

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,12 @@ module.exports = {
       name: "staffsync-backend",
       cwd: "/var/www/staffsync",
       script: "src/index.ts",
+      exec_mode: "fork",
+      instances: 1,
       interpreter: "node",
       node_args: "--max-old-space-size=4096 -r ts-node/register/transpile-only -r dotenv/config",
-      instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
       env: {
         NODE_ENV: "development",
         PORT: 3001
